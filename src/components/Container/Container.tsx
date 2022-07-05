289cf941ac9aa3013a5040865b89222529bb46af
g0m3s <gom3s.g@gmail.com>
2022-07-04 23:50:30 -0300

    feat: add footer with contact infos

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,8 +2,8 @@
 import { Dispatch, SetStateAction, useEffect } from 'react'
 import Lottie from 'react-lottie'
 import { useModal } from '../../utils/useModal'
-import { Stack, Typography } from '@mui/material'
 import heartAnimation from '../../assets/heart.json'
+import { Link, Stack, Typography } from '@mui/material'
 import { WelcomeModal, ProfileSection, DonateSection, Graphic } from './components'
 
 export interface ContainerProps {
@@ -42,6 +42,20 @@ export const Container: React.FC<ContainerProps> = ({ SetIsDonateModalOpen }) =>
       </Stack>
       <DonateSection SetIsDonateModalOpen={SetIsDonateModalOpen} />
       <Graphic />
+      <Stack
+        mt={4}
+        spacing={4}
+        paddingY={3}
+        width='100vw'
+        direction='row'
+        alignItems='center'
+        justifyContent='center'
+        bgcolor='secondary.main'
+      >
+        <Link target='_blank' sx={{ textDecoration: 'none' }} typography={{ xs: 'text7', lg: 'h4' }} href='https://www.linkedin.com/in/gabriel-gomes-a0681a157/'>Linkedin</Link>
+        <Link target='_blank' sx={{ textDecoration: 'none' }} typography={{ xs: 'text7', lg: 'h4' }} href='mailto:gom3s.g@gmail.com'>Gmail</Link>
+        <Link target='_blank' sx={{ textDecoration: 'none' }} typography={{ xs: 'text7', lg: 'h4' }} href='https://github.com/g0m3s/ze-delivery'>GitHub</Link>
+      </Stack>
     </Stack >
   )
 }
